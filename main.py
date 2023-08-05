1b14631837f73f4f93c874b7ca7969c145bd1d2a
William Massami <99924347+h4rad4@users.noreply.github.com>
2023-08-04 21:52:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 def atm(valor: int):
     valores = [100, 50, 10, 5, 1]
-    qtd = [0, 0, 0, 0, 0]
-    for i in range(0, 5):
-        while valor >= valores[i]:
-            qtd[i] += 1
-            valor -= valores[i]
+    qtd = []
+    for v in valores:
+        notas = valor // v
+        valor %= v
+        qtd.append(notas)
 
     return qtd
 
-
 valores = [100, 50, 10, 5, 1]
-valor = float(input("Qual o valor de saque? "))
+valor = int(input("Qual o valor de saque? "))
 qtd = atm(valor)
 
+print("Notas entregues:")
 for i in range(0, 5):
     if qtd[i] != 0 and qtd[i] > 1:
         print(f"{qtd[i]} notas de {valores[i]}", end=", ")
